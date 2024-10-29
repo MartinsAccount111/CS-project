05406352960dd074edf1205a22a1d20e0b58b45b
MartinsAccount111 <martin.drabik@student.unisg.ch>
2024-10-29 18:27:50 +0100

    .

--- projectname.py
+++ projectname.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.write("how do i get whole thing to streamlit, yeah")
 
-print("yeahhh budddyy")
-
+st.print("yeahhh budddyy")
+number = st.number_input("Enter a number:", min_value=0, max_value=100, value=10)
 
 
