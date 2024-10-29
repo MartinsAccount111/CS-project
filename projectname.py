b57042479ac86507f4cd47cc1c337ad7ff8af12a
MartinsAccount111 <martin.drabik@student.unisg.ch>
2024-10-29 18:31:12 +0100

    .

--- projectname.py
+++ projectname.py
@@ -6,8 +6,6 @@ number = st.number_input("Enter a number:", min_value=0, max_value=100, value=10
 # Performing calculations
 result = number * 2  # You can replace this with more complex logic if needed
 
-# Displaying output
-st.write("The result of multiplying your number by 2 is:", result)
 
 # Displaying output with a larger font size
 st.write("The result of multiplying your number by 2 is:")
